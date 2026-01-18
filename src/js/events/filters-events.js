291ff4bd57015e804c89fa66428dafab42b3a4f0
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2026-01-18 12:46:42 -0300

    ajuste no filtro de data da tabela

--- src/js/events/filters-events.js
+++ src/js/events/filters-events.js
@@ -1,12 +1,31 @@
 export function initFiltersEvents(filterController) {
+  const startInput = document.getElementById("filter-start");
+  const endInput = document.getElementById("filter-end");
+
   // eventos
   document.getElementById("filter-subject").addEventListener("change", () => {
     filterController.applyFilters();
   });
-  document.getElementById("filter-start").addEventListener("change", () => {
+
+  startInput.addEventListener("change", () => {
+    endInput.min = startInput.value;
+
+    if (endInput.value && endInput.value < startInput.value) {
+      endInput.value = startInput.value;
+    }
+
     filterController.applyFilters();
   });
-  document.getElementById("filter-end").addEventListener("change", () => {
+
+  endInput.addEventListener("change", () => {
+    if (endInput.value) {
+      startInput.max = endInput.value;
+    } else {
+      // Se limpar o fim, volta o max para "Hoje" (que é o padrão do sistema)
+      const today = new Date().toISOString().split("T")[0];
+      startInput.max = today;
+    }
+
     filterController.applyFilters();
   });
 
@@ -17,6 +36,10 @@ export function initFiltersEvents(filterController) {
   document
     .getElementById("btn-clear-filter-history")
     .addEventListener("click", () => {
+      const today = new Date().toISOString().split("T")[0];
+      endInput.min = "";
+      startInput.max = today;
+
       filterController.clearFilters();
     });
 }
